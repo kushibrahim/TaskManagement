055593d7604ee52f95bed516d62f04d3ee986e6e
Mesut ORMANLI <mormanli@ebay.com>
2020-01-29 11:28:12 +0300

    updates metricConverter

--- src/main/java/com/kushibrahim/taskmanagement/service/converter/MetricConverter.java
+++ src/main/java/com/kushibrahim/taskmanagement/service/converter/MetricConverter.java
@@ -3,35 +3,39 @@ package com.kushibrahim.taskmanagement.service.converter;
 import com.kushibrahim.taskmanagement.model.dto.MetricDto;
 import com.kushibrahim.taskmanagement.model.entity.MetricEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class MetricConverter {
 
     public List<MetricDto> convertListMetricDto(List<MetricEntity> metricEntities) {
-        if(!(metricEntities.size()>0)){
-            return null;
-        }
-        else {
-            MetricDto metricDto = new MetricDto();
-            for(MetricEntity metricEntity : metricEntities){
+        if (CollectionUtils.isEmpty(metricEntities)) {
+            return Collections.emptyList();
+        } else {
+            List<MetricDto> metricDtos = new ArrayList<>();
+            for (MetricEntity metricEntity : metricEntities) {
+                MetricDto metricDto = new MetricDto();
                 metricDto.setMetricID(metricEntity.getMetricID());
                 metricDto.setTaskID(metricEntity.getTaskID());
                 metricDto.setMetricStartDate(metricEntity.getMetricStartDate());
                 metricDto.setMetricOriginalEndDate(metricEntity.getMetricOriginalEndDate());
                 metricDto.setMetricActualEndDate(metricEntity.getMetricActualEndDate());
                 metricDto.setMetricType(metricEntity.getMetricType());
+                metricDtos.add(metricDto);
 
             }
-            return (List<MetricDto>) metricDto;
+            return metricDtos;
         }
     }
 
     public MetricDto convertMetricDto(MetricEntity metricEntity) {
-        if(metricEntity == null){
+        if (metricEntity == null) {
             return null;
-        }
-        else {
+        } else {
             MetricDto metricDto = new MetricDto();
             metricDto.setMetricType(metricEntity.getMetricType());
             metricDto.setMetricActualEndDate(metricEntity.getMetricActualEndDate());
