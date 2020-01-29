2c73c0148d528f0968aa6b3d14e1062b67423e30
kushibrahim <kushibrahim@gmail.com>
2020-01-29 11:16:49 +0300

    refactors project structure

---
+++ src/main/java/com/kushibrahim/taskmanagement/repository/impl/TaskRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.kushibrahim.taskmanagement.repository.impl;
+
+import com.kushibrahim.taskmanagement.model.entity.TaskEntity;
+import com.kushibrahim.taskmanagement.repository.TaskRepository;
+import org.hibernate.Session;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+
+@Repository
+public class TaskRepositoryImpl extends SimpleJpaRepository<TaskEntity, Integer> implements TaskRepository {
+
+    private final EntityManager entityManager;
+
+    public TaskRepositoryImpl(EntityManager entityManager) {
+        super(TaskEntity.class, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public TaskEntity assigneeTask(Integer taskId, Integer assigneeId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        //return assigneeTask(taskId,assigneeId);
+        return null;
+    }
+
+    @Override
+    public TaskEntity updateTask(Integer taskId) {
+        Session session = entityManager.unwrap(Session.class);
+        TaskEntity taskEntity = session.get(TaskEntity.class,taskId);
+        session.saveOrUpdate(taskEntity);
+        return taskEntity;
+    }
+}
