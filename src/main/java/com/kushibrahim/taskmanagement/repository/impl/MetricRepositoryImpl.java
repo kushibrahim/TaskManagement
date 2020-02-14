14a2a80ce82895f5af19075cdafc02948add8ef8
kushibrahim <kushibrahim@gmail.com>
2020-02-14 13:57:58 +0300

    added MetricRepositoryImpl

---
+++ src/main/java/com/kushibrahim/taskmanagement/repository/impl/MetricRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.kushibrahim.taskmanagement.repository.impl;
+
+import com.kushibrahim.taskmanagement.model.entity.MetricEntity;
+import com.kushibrahim.taskmanagement.repository.MetricRepository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MetricRepositoryImpl extends SimpleJpaRepository<MetricEntity,Integer> implements MetricRepository {
+
+    private EntityManager entityManager;
+
+    public MetricRepositoryImpl(EntityManager entityManager) {
+        super(MetricEntity.class, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<MetricEntity> getAllDoneSuccessMetrics() {
+        List<MetricEntity> entities = this.findAll();
+        return entities.stream()
+                .filter(x -> x.getMetricType().equals("DONE"))
+                .collect(Collectors.toList());
+    }
+}
