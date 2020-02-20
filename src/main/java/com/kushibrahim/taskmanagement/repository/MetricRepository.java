edc914046bfacee725205d2f090c9b87f45d040c
kushibrahim <kushibrahim@gmail.com>
2020-02-20 11:05:25 +0300

    updated MetricRepository and ProcessRepository

--- src/main/java/com/kushibrahim/taskmanagement/repository/MetricRepository.java
+++ src/main/java/com/kushibrahim/taskmanagement/repository/MetricRepository.java
@@ -1,6 +1,7 @@
 package com.kushibrahim.taskmanagement.repository;
 
 import com.kushibrahim.taskmanagement.model.entity.MetricEntity;
+import com.kushibrahim.taskmanagement.model.entity.ProcessEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/kushibrahim/taskmanagement/repository/ProcessRepository.java
+++ src/main/java/com/kushibrahim/taskmanagement/repository/ProcessRepository.java
@@ -1,9 +1,17 @@
 package com.kushibrahim.taskmanagement.repository;
 
 import com.kushibrahim.taskmanagement.model.entity.ProcessEntity;
+import com.kushibrahim.taskmanagement.model.enumerator.MetricType;
+import com.kushibrahim.taskmanagement.model.enumerator.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.*;
 
 @Repository
 public interface ProcessRepository extends JpaRepository<ProcessEntity,Integer> {
+
+    @Query("select p from ProcessEntity p JOIN p.tasks t JOIN t.metrics m where t.taskActualEndDate > t.taskOriginalEndDate and m.metricType = :metricType and p.processStatus <> :processStatus")
+    List<ProcessEntity> processDone(MetricType metricType, Status processStatus);
 }
