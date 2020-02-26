50035ab21ac81d7451312f5acd8edda46a8e1e65
kushibrahim <kushibrahim@gmail.com>
2020-02-26 15:58:06 +0300

    added OverDueTaskControllerTest and updated OverDueTaskController

--- src/main/java/com/kushibrahim/taskmanagement/controller/OverDueTaskController.java
+++ src/main/java/com/kushibrahim/taskmanagement/controller/OverDueTaskController.java
@@ -2,6 +2,7 @@ package com.kushibrahim.taskmanagement.controller;
 
 import com.kushibrahim.taskmanagement.model.dto.TaskDto;
 import com.kushibrahim.taskmanagement.service.TaskService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,6 @@ public class OverDueTaskController {
 
     @GetMapping("/tasks")
     public ResponseEntity<List<TaskDto>> getOverDueTasks(){
-        return ResponseEntity.ok(taskService.getAllOverDueTask());
+        return taskService.getAllOverDueTask();
     }
 }
---
+++ src/test/java/com/kushibrahim/taskmanagement/controller/OverDueTaskControllerTest.java
@@ -0,0 +1,43 @@
+package com.kushibrahim.taskmanagement.controller;
+
+import com.kushibrahim.taskmanagement.base.BaseControllerTest;
+import com.kushibrahim.taskmanagement.model.dto.TaskDto;
+import com.kushibrahim.taskmanagement.service.impl.TaskServiceImpl;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@WebMvcTest(value = OverDueTaskControllerTest.class)
+public class OverDueTaskControllerTest extends BaseControllerTest {
+
+    private ResponseEntity<TaskDto> taskDto;
+    private ResponseEntity<List<TaskDto>> taskDtos;
+    private List<TaskDto> taskDtoList;
+
+    @MockBean
+    private TaskServiceImpl taskService;
+
+    @Test
+    public void whenGetOverDueTask_thenReturnListTaskDtoSuccess() throws Exception {
+        taskDtoList = Collections.singletonList(new TaskDto());
+        taskDtos = new ResponseEntity<List<TaskDto>>(taskDtoList, HttpStatus.OK);
+        when(taskService.getAllOverDueTask()).thenReturn(taskDtos);
+        mockMvc.perform(get("/overdue/tasks"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(taskService, times(1)).getAllOverDueTask();
+        verifyNoMoreInteractions(taskService);
+    }
+}
