ee2a09b6873ba08e044e7f62a74b036ad319c3f0
kushibrahim <kushibrahim@gmail.com>
2020-02-27 14:35:26 +0300

    updated MetricConverterTest and ProcessConverterTest.

--- src/test/java/com/kushibrahim/taskmanagement/model/converter/MetricConverterTest.java
+++ src/test/java/com/kushibrahim/taskmanagement/model/converter/MetricConverterTest.java
@@ -1,12 +1,17 @@
 package com.kushibrahim.taskmanagement.model.converter;
 
 import com.kushibrahim.taskmanagement.model.dto.MetricDto;
+import com.kushibrahim.taskmanagement.model.entity.MetricEntity;
+import com.kushibrahim.taskmanagement.model.enumerator.MetricType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(SpringExtension.class)
@@ -23,6 +28,31 @@ public class MetricConverterTest {
     void convertToDtoNull(){
         final MetricDto metricDto = metricConverter.convertMetricDto(null);
         assertNull(null);
+    }
+
+    @Test
+    void convertDtoTest(){
+        MetricEntity metricEntity = new MetricEntity();
+        metricEntity.setMetricID(1);
+        metricEntity.setMetricType(MetricType.DONE);
+        metricEntity.setMetricActualEndDate(new Date("2020/12/12"));
+        metricEntity.setMetricOriginalEndDate(new Date("2020/12/16"));
+        metricEntity.setMetricStartDate(new Date("2020/12/01"));
+
+        MetricDto metricDto = new MetricDto();
+        metricDto.setMetricID(1);
+        metricDto.setMetricType(MetricType.DONE);
+        metricDto.setMetricActualEndDate(new Date("2020/12/12"));
+        metricDto.setMetricOriginalEndDate(new Date("2020/12/16"));
+        metricDto.setMetricStartDate(new Date("2020/12/01"));
+
+        MetricDto metricNewDto = metricConverter.convertMetricDto(metricEntity);
+
+        assertEquals(metricNewDto.getMetricID(), metricDto.getMetricID());
+        assertEquals(metricNewDto.getMetricType(), metricDto.getMetricType());
+        assertEquals(metricNewDto.getMetricActualEndDate(), metricDto.getMetricActualEndDate());
+        assertEquals(metricNewDto.getMetricStartDate(), metricDto.getMetricStartDate());
+        assertEquals(metricNewDto.getMetricOriginalEndDate(), metricDto.getMetricOriginalEndDate());
     }
 
     @AfterEach
--- src/test/java/com/kushibrahim/taskmanagement/model/converter/ProcessConverterTest.java
+++ src/test/java/com/kushibrahim/taskmanagement/model/converter/ProcessConverterTest.java
@@ -1,12 +1,15 @@
 package com.kushibrahim.taskmanagement.model.converter;
 
 import com.kushibrahim.taskmanagement.model.dto.ProcessDto;
+import com.kushibrahim.taskmanagement.model.entity.ProcessEntity;
+import com.kushibrahim.taskmanagement.model.enumerator.Status;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(SpringExtension.class)
@@ -23,6 +26,32 @@ public class ProcessConverterTest {
     void convertToDtoNull(){
         final ProcessDto processDto = processConverter.convertProcessDto(null);
         assertNull(null);
+    }
+
+    @Test
+    void convertDtoTest(){
+        ProcessEntity processEntity = new ProcessEntity();
+        processEntity.setProcessName("EXAMPLE");
+        processEntity.setProcessStatus(Status.TODO);
+        processEntity.setProcessAssignee(1);
+        processEntity.setProcessAssigneer(1);
+        processEntity.setProcessID(1);
+
+        ProcessDto processDto = new ProcessDto();
+        processDto.setProcessName("EXAMPLE");
+        processDto.setProcessStatus(Status.TODO);
+        processDto.setProcessAssignee(1);
+        processDto.setProcessAssigneer(1);
+        processDto.setProcessID(1);
+
+        ProcessDto processNewDto = processConverter.convertProcessDto(processEntity);
+
+        assertEquals(processNewDto.getProcessName(), processDto.getProcessName());
+        assertEquals(processNewDto.getProcessStatus(), processDto.getProcessStatus());
+        assertEquals(processNewDto.getProcessAssignee(), processDto.getProcessAssignee());
+        assertEquals(processNewDto.getProcessAssigneer(), processDto.getProcessAssigneer());
+        assertEquals(processNewDto.getProcessID(), processDto.getProcessID());
+
     }
 
     @AfterEach
